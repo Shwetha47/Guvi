ee5354b4a0ffbf427edbcf3c2f115721afb7a7ed
Shwetha47 <rsshwetha1999@gmail.com>
2019-06-11 13:46:13 +0530

    Create PosAdd.c

---
+++ PosAdd.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int a[100],i,j,k;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+scanf("%d",&a[i]);
+for(i=0;i<n;i++)
+{
+for(j=i+1;j<n;j++)
+{
+for(k=j+1;k,n;k++)
+{
+if(a[i]+a[j]==a[k])
+printf("%d %d %d",a[i],a[j],a[k]);
+}
+}
+}
+}
