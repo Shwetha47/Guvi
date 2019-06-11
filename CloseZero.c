f809c643bbc9f55fa4375adb9df06a96dfa57eb0
Shwetha47 <rsshwetha1999@gmail.com>
2019-06-11 13:53:27 +0530

    Create CloseZero.c

---
+++ CloseZero.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int a[100],i,j;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+scanf("%d",&a[i]);
+for(i=0;i<n;i++)
+{
+for(j=i+1;j<n;j++)
+{
+if(a[i]+a[j]==0||a[i]+a[j]==-1||a[i]+a[j]==1)
+printf("%d",a[i]+a[j]);
+}
+}
+}
