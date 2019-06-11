10aeebaef9350d6bad4901cc9e171e268256eb60
Shwetha47 <rsshwetha1999@gmail.com>
2019-06-11 14:03:27 +0530

    Create SubSet.c

---
+++ SubSet.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+int n,m,a[100],b[100],i,j,k,flag;
+scanf("%d %d",&n,&m);
+for(i=0;i<n;i++)
+scanf("%d",&a[i]);
+for(i=0;i<m;i++)
+scanf("%d",&b[i]);
+for(i=0;i<m;i++)
+{flag=1;
+for(j=0;j<n;j++)
+{
+if(a[j]==a[i])
+{
+flag=0;
+break;
+}
+if(flag==1)
+{
+break;
+}
+}
+if(flag==0)
+printf("YES");
+else printf("NO");
+}
+
