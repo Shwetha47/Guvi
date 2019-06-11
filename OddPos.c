e4be9f21ce2a40795e0526ba74b16cee0df19a84
Shwetha47 <rsshwetha1999@gmail.com>
2019-06-11 13:40:08 +0530

    Update OddPos.c

--- OddPos.c
+++ OddPos.c
@@ -2,13 +2,13 @@
 int main()
 {
 
-long long int a[1000],n,i,j;
-scanf("%lld",&n);
+long int a[1000],n,i;
+scanf("%ld",&n);
 for(i=0;i<n;i++)
-scanf("%lld",&a[i]);
+scanf("%ld",&a[i]);
 for(i=0;i<n;i++)
 {
 if((i%2==0&&a[i]%2==1)||(i%2==1&&a[i]%2==0))
-printf("%lld ",a[i]);
+printf("%ld ",a[i]);
 }
 }
