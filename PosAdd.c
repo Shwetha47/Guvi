0a98a33b4a4404b98672bfcee497f4694b48c4ae
Shwetha47 <rsshwetha1999@gmail.com>
2019-06-11 13:48:52 +0530

    Update PosAdd.c

--- PosAdd.c
+++ PosAdd.c
@@ -1,15 +1,17 @@
 #include<stdio.h>
 int main()
 {
-int a[100],i,j,k;
+int a[100],i,j,k,n;
 scanf("%d",&n);
 for(i=0;i<n;i++)
+{
 scanf("%d",&a[i]);
+}
 for(i=0;i<n;i++)
 {
 for(j=i+1;j<n;j++)
 {
-for(k=j+1;k,n;k++)
+for(k=j+1;k<n;k++)
 {
 if(a[i]+a[j]==a[k])
 printf("%d %d %d",a[i],a[j],a[k]);
