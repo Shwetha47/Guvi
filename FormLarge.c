41fea28776977a8fa9d2f009f6c03b9aa11f6606
Shwetha47 <rsshwetha1999@gmail.com>
2019-06-11 11:44:51 +0530

    Create FormLarge.c

---
+++ FormLarge.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+long long int n,a[1000],i,j,t;
+scanf("%lld",&n);
+for(i=0;i<n;i++)
+{
+scanf("%lld",&a[i]);
+}
+
+for(i=0;i,n;i++)
+{
+for(j=i+1;j<n;j++)
+{
+if(a[i]<a[j])
+{
+t=a[i];
+a[i]=a[j];
+a[j]=t;
+}
+}
+}
+for(i=0;i<n;i++)
+printf("%lld",a[i]);
+}
