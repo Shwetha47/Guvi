3e634a7e0f7285d9869e9204ec6c870118d579d0
Shwetha47 <rsshwetha1999@gmail.com>
2019-06-11 13:32:53 +0530

    Create repea.c

---
+++ repea.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+long long int n,a[1000],i,j,flag=0;
+scanf("%lld",&n);
+for(i=0;i<n;i++)
+scanf("%lld",&a[i]);
+for(i=0;i<n;i++)
+{
+
+for(j=i+1;j<n;j++)
+{
+if(a[i]==a[j])
+{
+flag=1;
+printf("%lld",a[i]);
+break;
+}
+}
+if(flag==1)
+  break;
+}
+  if(flag==0)
+printf("unique");
+}
